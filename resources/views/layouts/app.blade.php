0f0376f6cab6ecfe8f4e7059d914c6b5d9768607
Dayanitaaaa <dayanalizeth067@gmail.com>
2026-07-02 12:19:16 -0500

    Branding: cambio de nombre de la pestaña y logo favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Mas Que Perros' }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/huellita.png') }}">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
